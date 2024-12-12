8153552d4a13d706f4d91bc41dcf0a37531f6129
Ivan <yavorin@ukr.net>
2024-12-12 08:56:56 +0200

    adding initial Xcode project

---
+++ CurrencyConverterApp/CurrencyConverterApp/CurrencyConverterAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyConverterAppApp.swift
+//  CurrencyConverterApp
+//
+//  Created by Ivan Yavorin on 12.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyConverterAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
